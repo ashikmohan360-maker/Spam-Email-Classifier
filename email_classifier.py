39c312081bb14a4c5db0ef8580cd0765ea514256
Ashik Mohan <ashikmohan360@gmail.com>
2026-02-09 21:13:44 +0530

    Add files via upload

---
+++ email_classifier.py
@@ -0,0 +1,30 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+emails = [
+    "win money now",
+    "free prize claim now",
+    "limited offer buy now",
+    "you won a lottery",
+    "claim your free reward",
+    "meeting at 10 am",
+    "project discussion tomorrow",
+    "lunch with team",
+    "schedule a call",
+    "weekly report attached"
+]
+
+labels = [1,1,1,1,1,0,0,0,0,0]  # 1=Spam, 0=Not Spam
+
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(emails)
+
+model = MultinomialNB()
+model.fit(X, labels)
+
+# Test email
+test_email = ["free"]
+test_vec = vectorizer.transform(test_email)
+
+prediction = model.predict(test_vec)[0]
+print("Spam" if prediction == 1 else "Not Spam")
